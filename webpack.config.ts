706a30fc1ce2a33bc6ad5e2e126d00654406038d
maker27 <evgeniy@maker27.ru>
2021-07-19 15:09:48 +0300

    prepare project

---
+++ webpack.config.ts
@@ -0,0 +1,73 @@
+import path from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import ESLintPlugin from 'eslint-webpack-plugin';
+import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
+
+interface Configuration extends webpack.Configuration {
+    devServer?: WebpackDevServerConfiguration;
+}
+
+const config: Configuration = {
+    mode: 'development',
+    output: {
+        path: path.join(__dirname, '/public/'),
+        filename: '[name].js',
+        publicPath: '/'
+    },
+    entry: './src/index.tsx',
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript']
+                    }
+                }
+            },
+            {
+                test: /\.(css|s[ac]ss)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|jpe?g|gif|webp|ico|svg)$/i,
+                use: [
+                    {
+                        loader: 'file-loader'
+                    }
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.tpl.html',
+            inject: 'body',
+            filename: 'index.html'
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new ForkTsCheckerWebpackPlugin({
+            async: false
+        }),
+        new ESLintPlugin({
+            extensions: ['js', 'jsx', 'ts', 'tsx']
+        })
+    ],
+    devtool: 'eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'build'),
+        historyApiFallback: true,
+        port: 8000,
+        open: true,
+        hot: true
+    }
+};
+
+export default config;
